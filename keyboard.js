0892fc487c29056f525055604375e4f4129dd80c
Pal <palermuke@hotmail.com>
2015-05-25 11:18:57 +1200

    kb added keywraper
    for future responsive kb

--- keyboard.js
+++ keyboard.js
@@ -85,9 +85,11 @@ var createKey = function(key){
 			break;
 	}
 	*/
-	 document.writeln('<div id="k3d'+key+'" class="key3d" onclick="keyPress(event)">');
-	 document.writeln('  <div id="k2d'+key+'" class="key2d">');
-	 document.writeln('    <p class="keylabel">'+key+'</p>');
-	 document.writeln('  </div>');
+	 document.writeln('<div class="keywrapper">');
+	 document.writeln('	<div id="k3d'+key+'" class="key3d" onclick="keyPress(event)">');
+	 document.writeln('  	<div id="k2d'+key+'" class="key2d">');
+	 document.writeln('    		<p class="keylabel">'+key+'</p>');
+	 document.writeln(' 	</div>');
+	 document.writeln('	</div>');
 	 document.writeln('</div>');
 }  
