58838b03422f773e4640bb4cfcd2fcac21846aa9
Pal <palermuke@hotmail.com>
2015-05-26 14:54:44 +1200

    indentation. converted all to spaces
    no tabs

--- keyboard.js
+++ keyboard.js
@@ -7,7 +7,7 @@
 
 var kbLayout = []; 
 kbLayout.constantkb=29; //constant for creating keyboard.  ==(screen_width/key_width) 
-kbLayout.keysize=44; 	//keysize is 44 by default  used by several functions
+kbLayout.keysize=44;    //keysize is 44 by default  used by several functions
 kbLayout.windowX=1200;   //size of the window
 
 kbLayout.shadowTop="#ddd";
@@ -43,7 +43,7 @@ function getSizeWindow(){
     //var y = w.innerHeight|| e.clientHeight|| g.clientHeight;
     //console.log("x="+x);
     kbLayout.windowX=x;
-	kbLayout.keysize = parseInt(kbLayout.windowX/kbLayout.constantkb); 
+    kbLayout.keysize = parseInt(kbLayout.windowX/kbLayout.constantkb); 
     return x;
 }
 
@@ -52,120 +52,120 @@ function getSizeWindow(){
 
 
 var createKeyboard = function (type){
-	if (type == "ENG") {   //so far only english kb
-		}else {  type="ENG"; }
-	getSizeWindow();
+    if (type == "ENG") {   //so far only english kb
+        }else {  type="ENG"; }
+    getSizeWindow();
 
-	var	auxStyle1="style='"+
-			      "border-radius:"+kbLayout.keysize/2+"px;"+
-				  "padding:"+kbLayout.keysize+"px;"+
-		          " ' ";
-	var auxStyle2="style='"+
-				 "height:"+(kbLayout.keysize+1)+"px;"+
-				 " ' "; // for storing the style		
-	var kbWrap = document.getElementById('kbWrap');
-	kbWrap.innerHTML +='<div id="keyboard" class="center" '+auxStyle1+'></div>'; 
-	var keyboard=document.getElementById("keyboard");        
-//	document.writeln('<div id="keyboard" class="center" '+auxStyle1+'>');
-	for (var g=0,h=kbLayout[type].length;g<h;g++){  //rows
-		keyboard.innerHTML +=("<div  id='kbRow" + g + "' class='kbRow' "+auxStyle2+" ></div>");
-		var kbRow=document.getElementById("kbRow"+g);
-//		document.writeln("<div  id='kbRow" + g + "' class='kbRow' "+auxStyle2+" >");
-		for(var i=0,j=kbLayout[type][g].length;i<j;i++){
-			var aux=kbLayout[type][g][i];
-			if(isNaN(aux)){  //convert number to spaces between keys. This spaces are record as numbers
-				createKey(aux,kbRow);
-			} 	
-			else{
-				emptySpace(aux,kbRow);
-			}
-			//createKey(aux);
-		}
-	//	document.writeln("</div>");
-	}
-//	document.writeln("</div>");
-	
+    var auxStyle1="style='"+
+                  "border-radius:"+kbLayout.keysize/2+"px;"+
+                  "padding:"+kbLayout.keysize+"px;"+
+                  " ' ";
+    var auxStyle2="style='"+
+                 "height:"+(kbLayout.keysize+1)+"px;"+
+                 " ' "; // for storing the style        
+    var kbWrap = document.getElementById('kbWrap');
+    kbWrap.innerHTML +='<div id="keyboard" class="center" '+auxStyle1+'></div>'; 
+    var keyboard=document.getElementById("keyboard");        
+//  document.writeln('<div id="keyboard" class="center" '+auxStyle1+'>');
+    for (var g=0,h=kbLayout[type].length;g<h;g++){  //rows
+        keyboard.innerHTML +=("<div  id='kbRow" + g + "' class='kbRow' "+auxStyle2+" ></div>");
+        var kbRow=document.getElementById("kbRow"+g);
+//      document.writeln("<div  id='kbRow" + g + "' class='kbRow' "+auxStyle2+" >");
+        for(var i=0,j=kbLayout[type][g].length;i<j;i++){
+            var aux=kbLayout[type][g][i];
+            if(isNaN(aux)){  //convert number to spaces between keys. This spaces are record as numbers
+                createKey(aux,kbRow);
+            }   
+            else{
+                emptySpace(aux,kbRow);
+            }
+            //createKey(aux);
+        }
+    //  document.writeln("</div>");
+    }
+//  document.writeln("</div>");
+    
 }
 
 
 
 var emptySpace =function(aux,kbRow){
-	var auxStyle=" style='width:"+(aux*kbLayout.keysize)+"px;'";
-	kbRow.innerHTML+=('<div class="keywrapper empty" '+auxStyle+'></div>');
-//	document.writeln('<div class="keywrapper empty" '+auxStyle+'>');
-//	document.writeln('</div>');
+    var auxStyle=" style='width:"+(aux*kbLayout.keysize)+"px;'";
+    kbRow.innerHTML+=('<div class="keywrapper empty" '+auxStyle+'></div>');
+//  document.writeln('<div class="keywrapper empty" '+auxStyle+'>');
+//  document.writeln('</div>');
 }
 var createKey = function(key,kbRow){
-	var aux="",  //for store the style     aux="style='+a +"xxx"+b+" ' ";
-	sizeW=1,   //sizeW 1= regular key,    sizeW=2 : 2 times  the sizeW of a regular key 
-	sizeH=1;
-	switch(key){
-		case "Tab":
-			sizeW=1.5;
-			break;
-		case "Backspace":
-			sizeW=2;
-			break;
-		case "|<br>\\":
-			sizeW=1.5;
-			break;
-		case "Caps Lock":
-			sizeW=1.75;
-			break;	
-		case "Enter ":  //added an space for distinguishing the two enter keys
-			sizeW=2.25;
-			break;
-		case "Shift":
-			sizeW=2.25;
-			break;	
-		case "Shift ": //added an space for distinguishing the two shifts keys
-			sizeW=2.75;
-			break;	
-		case "Ctrl":
-			sizeW=1.25;
-			break;	
-		case "Win":
-			sizeW=1.25;
-			break;	
-		case "Alt":
-			sizeW=1.25;
-			break;	
-		case "_":
-			sizeW=6.25;
-			break;	
-		case "Menu":
-			sizeW=1.25;
-			break;	
-		case "Enter":
-			sizeW=1;
-			break;	
-		default:
-			break;
-	}
-	aux=" style=' "+
-	    "width:"+(sizeW*kbLayout.keysize)+"px;"+
-	    "height:"+(sizeH*kbLayout.keysize)+"px;"+
-	    " ' ";
-	kbRow.innerHTML+=('<div class="keywrapper full" id="keywrapper'+key+'" '+aux+'></div>');
-	var keywrapper=document.getElementById("keywrapper"+key);   /****/
-//	document.writeln('<div class="keywrapper full" '+aux+'>');
-	aux=" style=' "+
+    var aux="",  //for store the style     aux="style='+a +"xxx"+b+" ' ";
+    sizeW=1,   //sizeW 1= regular key,    sizeW=2 : 2 times  the sizeW of a regular key 
+    sizeH=1;
+    switch(key){
+        case "Tab":
+            sizeW=1.5;
+            break;
+        case "Backspace":
+            sizeW=2;
+            break;
+        case "|<br>\\":
+            sizeW=1.5;
+            break;
+        case "Caps Lock":
+            sizeW=1.75;
+            break;  
+        case "Enter ":  //added an space for distinguishing the two enter keys
+            sizeW=2.25;
+            break;
+        case "Shift":
+            sizeW=2.25;
+            break;  
+        case "Shift ": //added an space for distinguishing the two shifts keys
+            sizeW=2.75;
+            break;  
+        case "Ctrl":
+            sizeW=1.25;
+            break;  
+        case "Win":
+            sizeW=1.25;
+            break;  
+        case "Alt":
+            sizeW=1.25;
+            break;  
+        case "_":
+            sizeW=6.25;
+            break;  
+        case "Menu":
+            sizeW=1.25;
+            break;  
+        case "Enter":
+            sizeW=1;
+            break;  
+        default:
+            break;
+    }
+    aux=" style=' "+
+        "width:"+(sizeW*kbLayout.keysize)+"px;"+
+        "height:"+(sizeH*kbLayout.keysize)+"px;"+
+        " ' ";
+    kbRow.innerHTML+=('<div class="keywrapper full" id="keywrapper'+key+'" '+aux+'></div>');
+    var keywrapper=document.getElementById("keywrapper"+key);   /****/
+//  document.writeln('<div class="keywrapper full" '+aux+'>');
+    aux=" style=' "+
         "border-top:"+kbLayout.shadowTop+" "+parseInt(kbLayout.keysize/10)+"px solid;"+
         "border-left:"+kbLayout.shadowLeft+" "+parseInt(kbLayout.keysize/7)+"px solid;"+
         "border-bottom:"+kbLayout.shadowBottom+" "+parseInt(kbLayout.keysize/5)+"px solid;"+
         "border-right:"+kbLayout.shadowRight+" "+parseInt(kbLayout.keysize/7)+"px solid;"+
-        " ' ";	
-    keywrapper.innerHTML+=('  	<div id="k2d'+key+'" class="key2d" '+aux+'></div>');
+        " ' ";  
+    keywrapper.innerHTML+=('    <div id="k2d'+key+'" class="key2d" '+aux+'></div>');
     var key2d=document.getElementById("k2d"+key);
-//	document.writeln('  	<div id="k2d'+key+'" class="key2d" '+aux+'>');
-	aux=" style=' "+
-		"font-size:"+parseInt(kbLayout.keysize/4)+"px;"+
-		"top:"+parseInt(kbLayout.keysize/40)+"px;"+
-		"left:"+parseInt(kbLayout.keysize/40)+"px;"+
-		" ' ";
-	key2d.innerHTML+=('    		<p class="keylabel" '+aux+'>'+key+'</p>');
-//	document.writeln('    		<p class="keylabel" '+aux+'>'+key+'</p>');
-//	document.writeln(' 	</div>');
-	//document.writeln('	 </div>');
-//	document.writeln('</div>');
+//  document.writeln('      <div id="k2d'+key+'" class="key2d" '+aux+'>');
+    aux=" style=' "+
+        "font-size:"+parseInt(kbLayout.keysize/4)+"px;"+
+        "top:"+parseInt(kbLayout.keysize/40)+"px;"+
+        "left:"+parseInt(kbLayout.keysize/40)+"px;"+
+        " ' ";
+    key2d.innerHTML+=('         <p class="keylabel" '+aux+'>'+key+'</p>');
+//  document.writeln('          <p class="keylabel" '+aux+'>'+key+'</p>');
+//  document.writeln('  </div>');
+    //document.writeln('     </div>');
+//  document.writeln('</div>');
 }  
--- monkey.js
+++ monkey.js
@@ -3,44 +3,44 @@
 var totalStrokes = 0;
 
 var keyDown = function(event) {   //all capital letters
-	//getKey(event);
-	keyAnimation(getKey(event),1);
+    //getKey(event);
+    keyAnimation(getKey(event),1);
 }
 var keyPress = function(event) {  //distinguish capital letters and lowercase
-	totalStrokes++;
-	//console.log("keyPress");
-	getKey(event);
-	
+    totalStrokes++;
+    //console.log("keyPress");
+    getKey(event);
+    
 }
 var keyUp = function(event) {
-	//console.log("keyUp");
-	//keyAnimation(getKey(event),-1);
+    //console.log("keyUp");
+    //keyAnimation(getKey(event),-1);
 }
 
 
-	
+    
 var getKey = function (event) {
-    var x = event.which || event.keyCode;  	/* Firefox doesn't support event.which , then  use event.keyCode */
+    var x = event.which || event.keyCode;   /* Firefox doesn't support event.which , then  use event.keyCode */
     console.log("The Unicode value is: " + x);
     console.log(String.fromCharCode(x));
     return String.fromCharCode(x);
 }
 
 var keyAnimation = function(key,direction){
-	monkeyAnimation(direction);
+    monkeyAnimation(direction);
 }
 
 
 var monkeyAnimation = function (direction) {
-	console.log("in keyAnimation value = " + direction);
-	if (direction == 1){		//move arm down
-		document.getElementById("dinamic").style.transition= "none";
-		document.getElementById("dinamic").style.transform = "rotate(-1deg)";
+    console.log("in keyAnimation value = " + direction);
+    if (direction == 1){        //move arm down
+        document.getElementById("dinamic").style.transition= "none";
+        document.getElementById("dinamic").style.transform = "rotate(-1deg)";
 
-	} else {				//move arm up
-		document.getElementById("dinamic").style.transition = "all 0.5s ease-in-out";
-		document.getElementById("dinamic").style.transform = "rotate(+10deg)";
-	}
+    } else {                //move arm up
+        document.getElementById("dinamic").style.transition = "all 0.5s ease-in-out";
+        document.getElementById("dinamic").style.transform = "rotate(+10deg)";
+    }
     
 
 }
